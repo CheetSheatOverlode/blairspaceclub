d6e6e50dfecd25753f270ad6d1daab46f191c79c
Replit user <>
2023-12-23 16:48:51 +0000

    fixed indentation

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function copyToClipboard(text) {
-  const el = document.createElement('textarea');
-  el.value = text;
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
+	const el = document.createElement('textarea');
+  	el.value = text;
+  	document.body.appendChild(el);
+  	el.select();
+  	document.execCommand('copy');
+  	document.body.removeChild(el);
 }
